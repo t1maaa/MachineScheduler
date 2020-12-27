dbbcc522a5aa49c4c6f44b54bdc9e80b99e07655
t1maaa <t1maaa@mail.ru>
2020-12-28 03:21:27 +0700

    cleanup + some minor changes

--- KrasNonFerMet.Core/SimpleScheduler.cs
+++ KrasNonFerMet.Core/SimpleScheduler.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using KrasNonFerMet.DAL.Files.Excel;
 using KrasNonFerMet.DAL.Models;
 
 namespace KrasNonFerMet.Core
 {
     public class SimpleScheduler
     {
-        private List<MachineSchedule> MachineSchedules { get; set; }
+        private List<MachineSchedule> MachineSchedules { get; }
         public SimpleScheduler()
         {
             MachineSchedules = new List<MachineSchedule>();
@@ -22,7 +20,7 @@ namespace KrasNonFerMet.Core
             if (!machines?.Any() ?? true) throw new ArgumentException("Machines list null or empty");
             if (!nomenclatures?.Any() ?? true) throw new ArgumentException("Nomenclatures list null or empty");
             if (!operations?.Any() ?? true) throw new ArgumentException("Operations list null or empty");
-            
+
             foreach (var machine in machines)
             {
                 MachineSchedules.Add(new MachineSchedule(machine.Id));
@@ -41,8 +39,8 @@ namespace KrasNonFerMet.Core
                                          .Select(o => new {o.MachineId, o.Duration})
                                          .OrderBy(o => o.Duration)) 
                                  join ms in MachineSchedules on suitableMachines.MachineId equals ms.Id 
-                                 select new {suitableMachines.MachineId, suitableMachines.Duration, ms.LastJob.CompleteAt}) 
-                         select new {m.MachineId, StartAt = m.CompleteAt, CompleteAt = m.CompleteAt + m.Duration})
+                                 select new { suitableMachines.MachineId, suitableMachines.Duration, ms.LastJob.CompleteAt }) 
+                         select new Job { MachineId = m.MachineId, StartAt = m.CompleteAt, CompleteAt = m.CompleteAt + m.Duration,  ConsignmentId = consignment.Id })
                      .OrderBy(m => m.CompleteAt).First();
                  */
 
@@ -53,7 +51,8 @@ namespace KrasNonFerMet.Core
                 var machineCandidates = operations
                     .Where(o => o.NomenclatureId == consignment.NomenclatureId)
                     .Select(o => new { o.MachineId, o.Duration })
-                    .OrderBy(o => o.Duration).ToList();
+                    .OrderBy(o => o.Duration)
+                    .ToList();
 
                 var machineCandidatesSchedules = (from mc in machineCandidates
                     join ms in MachineSchedules on mc.MachineId equals ms.Id
@@ -64,34 +63,15 @@ namespace KrasNonFerMet.Core
 
                 var newJob = (from mclj in machineCandidatesLastJobs
                         join mc in machineCandidates on mclj.MachineId equals mc.MachineId
-                        select (new { mc.MachineId, StartAt = mclj.CompleteAt, CompleteAt = mc.Duration + mclj.CompleteAt }))
+                        select (new Job { MachineId = mc.MachineId, StartAt = mclj.CompleteAt, CompleteAt = mc.Duration + mclj.CompleteAt, ConsignmentId = consignment.Id}))
                     .OrderBy(s => s.CompleteAt).First();
 
                 #endregion
                 
-                //TODO: ^^ Test performance of both ^^
-
                 MachineSchedules.FirstOrDefault(ms => ms.Id == newJob.MachineId)
-                  ?.AddJob(new Job
-                  {
-                      CompleteAt = newJob.CompleteAt,
-                      ConsignmentId = consignment.Id,
-                      MachineId = newJob.MachineId,
-                      StartAt = newJob.StartAt
-                  });
+                  ?.AddJob(newJob);
             }
-            return MachineSchedules; //TODO: default order by consignment?
-        }
-        public void Export(FileInfo file, ExportMode mode = ExportMode.Merged)
-        {//TODO: Handle exceptions && null checks
-            IExcelSchedulesExport<MachineSchedule> export = new MachineSchedulesExport(MachineSchedules);
-            export.ExportMode = mode;
-            export.SaveAs(file);
-        }
-
-        public void OrderBy()
-        {
-            throw new NotImplementedException();
+            return MachineSchedules;
         }
     }
 }
